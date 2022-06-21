ae69b00a290760a690237e57b93ce61da947f242
MikhailLipanin <mikhail.lipanin@gmail.com>
2022-06-21 17:56:06 +0300

    0.0.0

---
+++ JumpEliminationTool/src/main/java/org/eolang/jump/Main.java
@@ -0,0 +1,10 @@
+package org.eolang.jump;
+
+import javax.xml.transform.TransformerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        TransformerFactory transformerFactory = TransformerFactory.newDefaultInstance();
+
+    }
+}
