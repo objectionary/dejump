eafe319eb67d95a96c1aeec1d037329cff805e9f
MikhailLipanin <mikhail.lipanin@gmail.com>
2022-06-26 19:36:47 +0300

    0

--- src/main/java/org/eolang/jump/Main.java
+++ src/main/java/org/eolang/jump/Main.java
@@ -1,7 +1,12 @@
 package org.eolang.jump;
 
+import com.jcabi.xml.XSLDocument;
+import com.yegor256.xsline.Xsline;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        //Xsline xsline = new Xsline(new XSLDocument());
     }
 }
