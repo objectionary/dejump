7202d0c8a2383ddae4d915f9324a1ed5e6a8f988
MikhailLipanin <mikhail.lipanin@gmail.com>
2022-07-05 13:47:17 +0300

    update .gitignore

--- src/main/java/org/eolang/jump/RemoveGOTO.java
+++ src/main/java/org/eolang/jump/RemoveGOTO.java
@@ -5,11 +5,14 @@ import com.jcabi.xml.XMLDocument;
 import com.jcabi.xml.XSLDocument;
 import com.yegor256.xsline.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class RemoveGOTO {
     public void exec() throws IOException {
-        final Train<Shift> train = new TrDefault<>()
+        final Train<Shift> train = new TrDefault<Shift>()
                 .with(new StXSL(new XSLDocument(this.getClass().getResource("add-brackets.xsl"))));
         
         XML input = new XMLDocument(this.getClass().getResource("test.xml"));
