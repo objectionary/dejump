6987795d3754ceb86e58557f0f40a061f65698d0
MikhailLipanin <mikhail.lipanin@gmail.com>
2022-07-20 19:17:50 +0300

    Bug fix

--- src/main/java/org/eolang/jump/Main.java
+++ src/main/java/org/eolang/jump/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        RemoveGOTO run = new RemoveGOTO();
-        run.exec(args[0]);
+        new RemoveGOTO(args[0]).exec();
     }
 }
--- src/main/java/org/eolang/jump/RemoveGOTO.java
+++ src/main/java/org/eolang/jump/RemoveGOTO.java
@@ -4,28 +4,64 @@ import com.jcabi.xml.XML;
 import com.jcabi.xml.XMLDocument;
 import com.jcabi.xml.XSLDocument;
 import com.yegor256.xsline.*;
+import org.cactoos.io.InputOf;
+import org.cactoos.io.OutputTo;
+import org.eolang.parser.Syntax;
+import org.eolang.parser.XMIR;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class RemoveGOTO {
-    public void exec(String path) throws IOException, ClassNotFoundException {
+    private final String path;
+    XML xmlIn;
+    XML xmlOut;
+    public RemoveGOTO(String pth) {
+        this.path = new File(pth).getAbsolutePath();
+    }
+    public void exec() throws IOException, ClassNotFoundException {
+        File curDir = new File(this.path.substring(0, this.path.lastIndexOf('\\')) + "\\generated");
+        String curName = new File(this.path).getName().substring(0, new File(this.path).getName().lastIndexOf('.'));
+        File input = new File(curDir.getPath() + '\\' + curName + ".xmir");
+
+        if (curDir.exists() && !deleteDirectory(curDir)) throw new IOException("Can't delete a 'generated' repository");
+        if (!(curDir.mkdir())) throw new IOException("Can't create a 'generated' repository");
+        if (!(input.createNewFile())) throw new IOException("Can't create an '../generated/*.xmir' file");
+
+        new Syntax(
+                this.path,
+                new InputOf(new File(this.path)),
+                new OutputTo(input)
+        ).parse();
         final Train<Shift> train = new TrDefault<Shift>()
-                .with(new StXSL(new XSLDocument(this.getClass().getResource("/SG.xsl"))));
-                //.with(new StXSL(new XSLDocument(this.getClass().getResource("/GF.xsl"))));
+                .with(new StXSL(new XSLDocument(this.getClass().getResource("/org/eolang/jump/SG.xsl"))))
+                .with(new StXSL(new XSLDocument(this.getClass().getResource("/org/eolang/jump/GF_1.xsl"))));
+                //.with(new StXSL(new XSLDocument(this.getClass().getResource("/org/eolang/jump/GF_2.xsl"))));
                 //.with(new StXSL(new XSLDocument(this.getClass().getResource("/GB.xsl"))));
                 //.with(new StXSL(new XSLDocument(this.getClass().getResource("/TW.xsl"))));
 
-        File in = new File(path);
-        XML input = new XMLDocument(in);
-        XML output = new Xsline(train).pass(input);
-
-        File ret = new File(path.substring(0, path.lastIndexOf('\\')) + "\\converted_" + in.getName());
-        ret.createNewFile();
-        try (FileWriter out = new FileWriter(ret)) {
-            out.write(output.toString());
+        this.xmlIn = new XMLDocument(input);
+        this.xmlOut = new Xsline(train).pass(xmlIn);
+        String ret = new XMIR(xmlOut).toEO();
+        File output = new File(curDir.getPath() + '\\' + curName + "_transformed.eo");
+        if (!(output.createNewFile())) throw new IOException("Can't create an '../generated/*_transformed.xmir' file");
+        try (FileWriter out = new FileWriter(output.getPath())) {
+            out.write(ret);
             out.flush();
         }
+    }
+    public static Boolean deleteDirectory(File directory) {
+        boolean ret = true;
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    ret &= deleteDirectory(file);
+                }
+            }
+        }
+        ret &= directory.delete();
+        return ret;
     }
 }
